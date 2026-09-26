0fe1377c02c4761a987847039743ce6c604e190e
Thibaut Rousseau <contact@thibaut-rousseau.com>
2026-09-26 16:46:42 +0200

    Remove examples

--- examples/hello/.ci/build/test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import os
-
-print("running tests...")
-print("CI            =", os.environ.get("CI"))
-print("PICI_WORKFLOW =", os.environ.get("PICI_WORKFLOW"))
-print("PICI_COMMIT_SHA =", os.environ.get("PICI_COMMIT_SHA"))
